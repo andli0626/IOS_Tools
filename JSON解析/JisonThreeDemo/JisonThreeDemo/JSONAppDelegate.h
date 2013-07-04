80f2942c5532d7ebedf771fdaf15d48ea62aa434
李林 <ww1095@163.com>
2013-07-04 10:03:16 +0800

    json解析

---
+++ JSON解析/JisonThreeDemo/JisonThreeDemo/JSONAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  JSONAppDelegate.h
+//  JisonThreeDemo
+//
+//  Created by rjxy on 12-8-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class JSONViewController;
+
+@interface JSONAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) JSONViewController *viewController;
+
+@end
---
+++ JSON解析/JisonThreeDemo/JisonThreeDemo/JSONViewController.h
@@ -0,0 +1,16 @@
+//
+//  JSONViewController.h
+//  JisonThreeDemo
+//
+//  Created by rjxy on 12-8-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSONViewController : UIViewController
+- (IBAction)buttonPressedone:(id)sender;
+- (IBAction)buttonPressedtwo:(id)sender;
+- (IBAction)buttonPressedthree:(id)sender;
+
+@end
